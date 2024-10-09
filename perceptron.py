d39283a8ce90bcad4f1b1ee1d757be8d4fcc9b99
alesaadi13 <al952016@gmail.com>
2024-10-10 00:29:07 +0330

    percepteron

--- perceptron.py
+++ perceptron.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 # Load CSV file into DataFrame
 file_path = './data/survey_lung_cancer_data.csv'
 df = pd.read_csv(file_path)
+# del df['AGE']
+# print(df)
 # new_col_value = 1
 # df.insert(loc=0,
 #           column='new-col',
@@ -17,6 +19,13 @@ df_target_value = df.iloc[0:, df.shape[1] - 1].values
 misclassified_value = df.isnull().sum()
 print("Missing values per column:\n", misclassified_value)
 
+df = pd.DataFrame(df)
+# print the original DataFrame
+print("Original DataFrame :")
+print(df)
+
+# shuffle the DataFrame rows
+df = df.sample(frac=1)
 df_train = df[:248]
 print('df_train: ', df_train)
 
@@ -26,24 +35,38 @@ print('df_test: ', df_test)
 train_examples = np.array(df_train.iloc[:, :])
 test_example = np.array(df_test.iloc[:, :])
 
-# weights
-w0 = -0.8
-random_state = 1
-rgen = np.random.RandomState(random_state)
-weights = rgen.normal(loc=0.0, scale=0.01, size=df.shape[1] - 1)
-weights = np.append(w0, weights)
+# weights:
+# Set the random seed for reproducibility
+np.random.seed(1)
+# Initialize weights
+n_features = df.shape[1] - 1  # Number of features in your dataset
+weights = np.random.uniform(low=-0.001, high=0.001, size=n_features)
+# add w0
+weights = np.append(-0.2, weights)
 delta_w = np.array([0.0 for i in range(len(weights))])
-rate = 0.1
-
+rates = [0.01, 0.02, 0.05, 0.07, 0.8]
 misclassified = 1
 misclassified_ = []
+correct_classified_ = []
+weights_Ranes = []
+performances_Rates_Weights_Train = []
+Train_Rates = []
+Train_Performance = []
+Train_Weights = []
+all_Train_weights_cuccess = []
+epochs = 5
+
+# the file path to save data
+csv_file_path = './data/weights_data.csv'
 
 
-def perceptron_algorithm(misclassified):
-
-    while misclassified != 0:
+def perceptron_algorithm(misclassified, rate):
+    # while misclassified != 0:
+    # weights_Train_cuccess = []
+    for epoch in range(epochs):
         misclassified = 0
-
+        correct_classified = 0
+        weights_Train_cuccess = []
         for i in range(len(train_examples)):
             sum = 0
             isinstance = train_examples[i]
@@ -57,7 +80,6 @@ def perceptron_algorithm(misclassified):
             target_value = isinstance[len(isinstance) - 1]
             if perceptron_output != target_value:
                 misclassified += 1
-                misclassified_.append(misclassified)
                 # delta_W0
                 delta_w[0] += rate * \
                     (target_value - perceptron_output)
@@ -68,53 +90,101 @@ def perceptron_algorithm(misclassified):
                                             perceptron_output) * isinstance[k]
                     # w_i = w_i + delta_w_i
                     weights[k+1] += delta_w[k+1]
-            break
-    if misclassified == 0:
-        print('success: ')
-        # print('perceptron delta_w: ', delta_w)
-        print('perceptron weights: ', weights)
-    else:
-        print('fail')
+            else:
+                if len(weights_Train_cuccess) == 0:
+                    correct_classified = 1
+                    weights_Train_cuccess.append(
+                        {'rate': rate, 'correct_classified': correct_classified, 'weights': tuple(weights)})
+                else:
+                    correct_classified += 1
+                    find_item = ()
+                    for n in range(len(weights_Train_cuccess)):
+                        item = weights_Train_cuccess[n]['weights']
+                        if np.array_equal(item, weights):
+                            find_item = item
+                            break
+                    if find_item:
+                        weights_Train_cuccess[n]['correct_classified'] = correct_classified
+                    else:
+                        weights_Train_cuccess.append(
+                            {'rate': rate, 'correct_classified': correct_classified, 'weights': tuple(weights)})
+    # all_Train_weights_cuccess.append(weights_Train_cuccess)
+    all_Train_weights_cuccess.append({rate: weights_Train_cuccess})
+
+    # Select the max performance classified
+    if len(weights_Train_cuccess) != 0:
+        max_Classified_Item = max(
+            weights_Train_cuccess, key=lambda x: x['correct_classified'])
+        max_performance = max_Classified_Item['correct_classified'] / \
+            len(train_examples) * 100.0
+        rate_max_Classified_Item = max_Classified_Item['rate']
+        weights_max_Classified_Item = max_Classified_Item['weights']
+        Train_Rates.append(rate_max_Classified_Item)
+        Train_Performance.append(max_performance)
+        Train_Weights.append(weights_max_Classified_Item)
+        performances_Rates_Weights_Train.append(
+            {'rate': rate_max_Classified_Item, 'performance': max_performance, 'weights': weights_max_Classified_Item})
+        # csv file for weights_Train_cuccess
+        weights_data = pd.DataFrame(performances_Rates_Weights_Train)
+        weights_data.to_csv(csv_file_path, index=False)
+
+# for x in performances_Rates_Weights_Train:
+#     Train_Rates.append(x['rate'])
+#     Train_Performance.append(x['performance'])
+#     Train_Weights.append(x['performance'])
 
 
 # Test
-weights_test = [-0.6,         0.21624345, 13.79388244,  0.19471828,  0.38927031,  0.40865408,
-                0.17698461,  0.21744812,  0.39238793,  0.20319039,  0.3975063,   0.41462108,
-                0.37939859,  0.39677583,  0.39615946,  0.41133769]
+weights_test = performances_Rates_Weights_Train
+performances_Test = []
+rates_Test = []
 
 
 def perceptron_test(weights_test):
-    misclassified_test = 0
-    classified_test = 0
 
-    for i in range(len(test_example)):
-        sum_test = 0
-        isinstance = test_example[i]
-        for j in range(len(test_example[i]) - 1):
-            sum_test += weights_test[j+1] * isinstance[j]
-        # w0_test
-        sum_test += weights_test[0]
-        if sum_test > 0:
-            perceptron_output_test = 1
-        else:
-            perceptron_output_test = -1
-        target_value = isinstance[len(isinstance) - 1]
-        if perceptron_output_test != target_value:
-            misclassified_test += 1
-        else:
-            classified_test += 1
-    print('success test: ', classified_test)
-    print('fail test: ', misclassified_test)
+    for k in range(len(weights_test)):
+        misclassified_test = 0
+        classified_test = 0
+        performance = 0
+        weight_t = weights_test[k]['weights']
+        rate_test = weights_test[k]['rate']
+        rates_Test.append(rate_test)
+
+        for i in range(len(test_example)):
+            sum_test = 0
+            isinstance_Test = test_example[i]
+            for j in range(len(isinstance_Test) - 1):
+                sum_test += weight_t[j+1] * isinstance_Test[j]
+            # w0_test
+            sum_test += weight_t[0]
+
+            if sum_test > 0:
+                perceptron_output_test = 1
+            else:
+                perceptron_output_test = -1
+
+            target_value_Test = isinstance_Test[len(isinstance_Test) - 1]
+            if perceptron_output_test != target_value_Test:
+                misclassified_test += 1
+            else:
+                classified_test += 1
+
+        performance = classified_test / len(test_example) * 100.0
+        performances_Test.append(performance)
 
 
-perceptron_algorithm(misclassified)
+# perceptron_algorithm
+for i in range(len(rates)):
+    perceptron_algorithm(misclassified, rates[i])
 perceptron_test(weights_test)
 
-# print('train_examples', len(train_examples.shape))
-# misclassified_ = np.array(misclassified_)
-# print('misclassified_', misclassified_.shape)
-# epochs = np.arange(1, len(train_examples))
-# plt.plot(epochs, misclassified_)
-# plt.xlabel('iterations')
-# plt.ylabel('misclassified')
-# plt.show()
+# Create a line plot
+plt.plot(Train_Rates, Train_Performance, label='Train', marker='o')
+plt.plot(rates_Test, performances_Test, label='Test', marker='o')
+# Add labels and title
+plt.xlabel('rates')
+plt.ylabel('performance')
+plt.title('Rate vs Performance Train')
+plt.legend()
+# Show the plot
+plt.show()
